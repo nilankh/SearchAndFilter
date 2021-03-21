18a1c34059a67b46935309574229e34a53e81e0b
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-03-21 13:06:10 +0530

    working on search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import JSONDATA from './MOCK_DATA.json';
 
 function App() {
   return (
     <div className="App">
       <input type="text" placeholder="Search..." />
+      {JSONDATA.map((val, key) => {
+        return <div>{val.product_name}</div>
+      })}
     </div>
   );
 }
