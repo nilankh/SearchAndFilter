1d85cfcccf48711fb9d92b03c4da952de0e78a26
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-03-21 13:15:40 +0530

    working on search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       {JSONDATA.map((val, key) => {
         return (
-          <div className="user" key = {key}>
+          <div className="user" key={key}>
             <p>{val.product_name}</p>
           </div>
         );
