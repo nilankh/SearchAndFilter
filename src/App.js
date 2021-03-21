46ce722c753dd74cb8bf003e6846d0487543a46b
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-03-21 13:08:41 +0530

    apply basic css so that all data should come in centre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
-import JSONDATA from './MOCK_DATA.json';
+import JSONDATA from "./MOCK_DATA.json";
 
 function App() {
   return (
     <div className="App">
       <input type="text" placeholder="Search..." />
       {JSONDATA.map((val, key) => {
-        return <div>{val.product_name}</div>
+        return (
+          <div className="user" key = {key}>
+            <p>{val.product_name}</p>
+          </div>
+        );
       })}
     </div>
   );
